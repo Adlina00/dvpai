bd8a3a9d953a40501a07dd15b3551f04df7a73c9
Adlina Amran <adlina.amran1902@gmail.com>
2024-03-26 22:37:58 +0800

    Update dvpai.py

--- dvpai.py
+++ dvpai.py
@@ -4,8 +4,7 @@ import pandas as pd
 import streamlit as st
 from openai import OpenAI
 import os
-import seaborn as sns
-import matplotlib as plt
+
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -26,6 +25,9 @@ def handle_data_upload_and_visual():
 
         columns = st.multiselect("Select columns for visualization", df.columns)
 
+        import seaborn as sns
+        import matplotlib as plt
+        
         if columns:
             #Generate a pairplot based on the selected columns
             st.write("Pairplot based on selected columns: ")
